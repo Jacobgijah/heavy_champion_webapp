6d82b80c4c98b2fd8f0c9dee488313834cc66ac3
Jacobgijah <jacobgijjah@gmail.com>
2024-04-13 17:37:23 +0300

    select all services and products

--- website/views.py
+++ website/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from website.models import *
 
 def index(request):
   return render(request, "pages/index.html")
@@ -10,8 +11,10 @@ def contact(request):
   return render(request, "pages/contact.html")
 
 def services(request):
-  return render(request, "pages/services.html")
+  services = Services.objects.all()
+  return render(request, "pages/services.html", {'services': services})
 
 def products(request):
-  return render(request, "pages/products.html")
+  products = Product.objects.all()
+  return render(request, "pages/products.html", {'products': products})
 

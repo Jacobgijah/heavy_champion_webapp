77d07b406159bc102bbb723d91b2234e70079cd6
Jacobgijah <jacobgijjah@gmail.com>
2024-04-13 18:06:57 +0300

    registering models

--- website/admin.py
+++ website/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+admin.site.register(models.Collection)
+
+admin.site.register(models.Product)
+
+admin.site.register(models.Services)
+
+admin.site.register(models.Promotion)

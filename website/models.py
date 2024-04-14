51591cadcee0a740a56102c97528a4a9876cceb3
Jacobgijah <jacobgijjah@gmail.com>
2024-04-14 16:00:09 +0300

    add slug column on product table

--- website/models.py
+++ website/models.py
@@ -19,12 +19,13 @@ class Collection(models.Model):
   
 class Product(models.Model):
   title = models.CharField(max_length=255)
+  slug = models.CharField(max_length=255, default='null')
   description = models.TextField()
   price = models.DecimalField(max_digits=6, decimal_places=2)
   inventory = models.IntegerField()
   last_update = models.DateTimeField(auto_now=True)
   collection = models.ForeignKey(Collection, on_delete=models.PROTECT)
-  promotions = models.ManyToManyField(Promotion, null=True, blank=True)
+  promotions = models.ManyToManyField(Promotion, blank=True)
   
   def __str__(self) -> str:
     return self.title

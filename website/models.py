d45a4726f4e537e46749e53691711289fb19b029
Jacobgijah <jacobgijjah@gmail.com>
2024-04-14 21:49:16 +0300

    adding data validation

--- website/models.py
+++ website/models.py
@@ -1,3 +1,4 @@
+from django.core.validators import MinValueValidator
 from django.db import models
 
 class Promotion(models.Model):
@@ -21,8 +22,11 @@ class Product(models.Model):
   title = models.CharField(max_length=255)
   slug = models.CharField(max_length=255)
   description = models.TextField()
-  price = models.DecimalField(max_digits=6, decimal_places=2)
-  inventory = models.IntegerField()
+  price = models.DecimalField(
+    max_digits=10,
+    decimal_places=2,
+    validators=[MinValueValidator(100)])
+  inventory = models.IntegerField(validators=[MinValueValidator(0)])
   last_update = models.DateTimeField(auto_now=True)
   collection = models.ForeignKey(Collection, on_delete=models.PROTECT)
   promotions = models.ManyToManyField(Promotion, blank=True)
